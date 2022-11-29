b37f67793d0f7ec6a7b70d8c7933a7cfcd2ea7fc
tomarsachin2271 <sachin.tomar@biconomy.io>
2022-11-30 01:32:22 +0530

    Remove contexts usage

--- ui/sdk-demo/typescript/src/index.tsx
+++ ui/sdk-demo/typescript/src/index.tsx
@@ -1,19 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Web3AuthProvider } from "./contexts/SocialLoginContext";
-import { SmartAccountProvider } from "./contexts/SmartAccountContext";
 
 const element = document.getElementById("root");
 const root = createRoot(element!);
 
 const Index = () => {
   return (
-    <Web3AuthProvider>
-      <SmartAccountProvider>
         <App />
-      </SmartAccountProvider>
-    </Web3AuthProvider>
   );
 };
 
